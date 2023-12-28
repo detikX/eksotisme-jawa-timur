a77f3f14a88208e4a3ff1c75598a5b6667340eb0
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-12-28 12:42:22 +0700

    kahdaj

--- js/ref.js
+++ js/ref.js
@@ -9,5 +9,10 @@ function ScrollIndicator() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById('footer__progress_bar').style.width = scrolled + "%";
     // document.getElementById('footer__progress_bar').innerHTML = Math.round(scrolled) + "%"
-    document.getElementById('footer__progress_bar').innerHTML = mobil
+    document.getElementById('footer__progress_bar').innerHTML = mobil;
+    if (Math.round(scrolled) > 40 && Math.round(scrolled) < 45) {
+        $(".bromo").addClass('gunung')
+    } else {
+        $(".bromo").removeClass('gunung')
+    }
 }
