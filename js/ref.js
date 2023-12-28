d6fccd04f658a502414b95656ebe13908208e495
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-12-28 11:28:39 +0700

    tes ref

---
+++ js/ref.js
@@ -0,0 +1,13 @@
+window.onscroll = function () { ScrollIndicator() };
+
+var mobil = "<div class='imgs'><img src='img/mobil-kanan.png' /></div>"
+function ScrollIndicator() {
+    // console.log(document.documentElement.scrollHeight);
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    // var height = $(".ref").height();
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById('footer__progress_bar').style.width = scrolled + "%";
+    // document.getElementById('footer__progress_bar').innerHTML = Math.round(scrolled) + "%"
+    document.getElementById('footer__progress_bar').innerHTML = mobil
+}
