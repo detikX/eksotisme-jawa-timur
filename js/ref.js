fc8a9cf72e96e161303b55efb13b0d28f121c3c0
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-12-29 14:09:44 +0700

    ded

--- js/ref.js
+++ js/ref.js
@@ -1,3 +1,10 @@
+$(document).ready(function () {
+    AOS.init();
+})
+
+//maen aos
+$(".mbl").attr({ "data-aos": "fade-right", "data-aos-delay": "100", "data-aos-duration": "500", "data-aos-offset": "10", "data-aos-easing": "linear" })
+
 window.onscroll = function () { ScrollIndicator() };
 
 var mobil = "<div class='imgs'><img src='img/mobil-kanan.png' /></div>"
