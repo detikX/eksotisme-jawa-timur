7c634e25b80cca9596b8cd1a58fad5e3584aa828
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-01-04 10:44:25 +0700

    ded

--- js/ref.js
+++ js/ref.js
@@ -50,3 +50,25 @@ $('.autoplay').slick({
     autoplay: true,
     autoplaySpeed: 3000,
 });
+
+
+$("#pulau-merah").on("shown.bs.modal", function (s) {
+    $(".slider-for").slick("setPosition"),
+        $(".slider-nav").slick("setPosition")
+}),
+    $(".slider-for").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: !0,
+        dots: !1,
+        arrows: !0,
+        asNavFor: ".slider-nav"
+    }),
+    $(".slider-nav").slick({
+        slidesToShow: 3,
+        arrows: !1,
+        slidesToScroll: 1,
+        asNavFor: ".slider-for",
+        centerMode: !1,
+        focusOnSelect: !0
+    })
