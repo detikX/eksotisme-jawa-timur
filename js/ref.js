9bc99ecf89d7b92bf3370bfa9c941af9d3ae94ff
dediwibisono17 <dedi.wibisono17@gmail.com>
2024-01-18 13:48:54 +0700

    deeded

--- js/ref.js
+++ js/ref.js
@@ -117,3 +117,52 @@ $("#pulau-merah").on("shown.bs.modal", function (s) {
 //     centerMode: true,
 //     focusOnSelect: true
 // });
+
+$("#djawatan").on("shown.bs.modal", function (s) {
+    $(".slider-for").slick("setPosition"),
+        $(".slider-nav").slick("setPosition")
+}),
+    $(".slider-for").slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: false,
+        arrows: true,
+        asNavFor: ".slider-nav"
+    }),
+    $(".slider-nav").slick({
+        slidesToShow: 3,
+        arrows: false,
+        slidesToScroll: 1,
+        asNavFor: ".slider-for",
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    })
